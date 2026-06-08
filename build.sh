1465927aa03e98f62f8ba587ba0f071fb8cfb2af
DESKTOP-58047GN\Maks- <maksimilian.suvajev@gmail.com>
2026-06-08 15:01:44 +0200

    Model loading clean build checkpoint, changed compiler to gcc (finally), also fixed build script in order to prevent accidental codebase pollutions.

--- build.sh
+++ build.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
+set -e
 
-# Basic build and clean functionality
-
-
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+BUILD_DIR="$SCRIPT_DIR/build"
 
 if [[ "$1" == "clean" || "$1" == "--clean" || "$1" == "-clean" || "$1" == "-c" || "$1" == "--c" ]]; then
-
-	echo "Performing clean build"
-	rm -rf build
-	rm -rf bin
-
-	echo "Making new build folder"
-	mkdir build
+    echo "Performing clean build"
+    rm -rf "$BUILD_DIR"
+    rm -rf "$SCRIPT_DIR/bin"
 fi
 
-echo "cd build"
-cd build
-echo "Running cmake"
-cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-cmake --build .
+mkdir -p "$BUILD_DIR"
+
+cmake -S "$SCRIPT_DIR" -B "$BUILD_DIR" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+                                       -DCMAKE_BUILD_TYPE=Debug \
+                                        -DCMAKE_VERBOSE_MAKEFILE=ON -G "MinGW Makefiles" \
+                                        -DCMAKE_PREFIX_PATH="C:/Qt/6.10.2/mingw_64"\
+                                         -DCMAKE_FIND_USE_PACKAGE_REGISTRY=OFF \
+                                         -DCMAKE_FIND_USE_SYSTEM_PACKAGE_REGISTRY=OFF
+
+
+                                        # -DQt6_DIR="C:/Qt/6.10.2/mingw_64/lib/cmake/Qt6" \
+                                        #-DCMAKE_C_COMPILER=gcc \
+                                        #-DCMAKE_CXX_COMPILER=g++ \
+
+cmake --build "$BUILD_DIR" -- -j$(nproc)
 
 exit 0
-
-
